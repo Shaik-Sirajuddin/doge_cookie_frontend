0ff4ce62d9e9b2159e683ca597d48db33381507c
kehinde cwb <85681159+codewithbillions@users.noreply.github.com>
2023-05-08 19:43:19 +0100

    starting presale page2

--- src/sections/Explore.jsx
+++ src/sections/Explore.jsx
@@ -18,7 +18,7 @@ const Explore = () => {
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
-        <TypingText title="| The Doge Cookieverse" textStyles="text-center" />
+        <TypingText title="| The Doge Cookie NFT" textStyles="text-center" />
         <TitleText
           title={<>Doge Cookie NFT  <br className="md:block hidden" />Marketplace</>}
           textStyles="text-center"
