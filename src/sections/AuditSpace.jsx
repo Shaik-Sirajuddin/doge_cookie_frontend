74657c53481f63061590f3a3b84dc1cc6d8e7741
irfan <irfan.mohammad@cognitivebotics.net>
2023-05-28 02:32:15 +0530

    updated audit space

--- src/sections/AuditSpace.jsx
+++ src/sections/AuditSpace.jsx
@@ -2,49 +2,28 @@ import React from 'react'
 import Card from 'react-bootstrap/Card';
 import { Row, Col } from 'react-bootstrap';
 export const AuditSpace = () => {
+    const reviews = [
+        { id: 1, name: 'UNISWAP', review: 'One of the Best.', imageUrl: 'https://picsum.photos/80/80?random=1' },
+        { id: 2, name: 'COINDCX', review: 'Safest among all.', imageUrl: 'https://picsum.photos/80/80?random=2' },
+        { id: 3, name: 'BINANCE', review: 'Profitable.', imageUrl: 'https://picsum.photos/80/80?random=3' },
+    ];
+
     return (
-        <div>
-            <h2> 100% Trusted and Audited by the following third parties:</h2>
-            <div className='container' style={{marginBlock:'10px'}}>
-                <Row>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="https://cdn.dribbble.com/userupload/3158903/file/original-3f5abe8b99ff4ba4626ddf6660115182.jpg?compress=1&resize=752x" />
-                            <Card.Body>
-                                <Card.Title>Secure</Card.Title>
-                                <Card.Text>
-                                Complete
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="https://cdn.dribbble.com/userupload/3158903/file/original-3f5abe8b99ff4ba4626ddf6660115182.jpg?compress=1&resize=752x" />
-                            <Card.Body>
-                                <Card.Title>Fast</Card.Title>
-                                <Card.Text>
-                                Complete
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="https://cdn.dribbble.com/userupload/3158903/file/original-3f5abe8b99ff4ba4626ddf6660115182.jpg?compress=1&resize=752x" />
-                            <Card.Body>
-                                <Card.Title>Reliable</Card.Title>
-                                <Card.Text>
-                                Complete
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    
-                </Row>
+        <div className="bg-yellow-200 py-10">
+            <div className="container mx-auto px-4">
+                <h2 className="text-2xl font-bold mb-6 text-center">100% Trusted and Audited by the following third parties:</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {reviews.map((review) => (
+                        <div key={review.id} className="bg-white p-4 shadow-md rounded-xl flex">
+                            <img src={review.imageUrl} alt={review.name} className="w-16 h-16 object-cover rounded-full mr-4" />
+                            <div>
+                                <h3 className="text-lg font-bold mb-2">{review.name}</h3>
+                                <p className="text-gray-700">{review.review}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-    )
-}
-
-// export default AuditSpace
+    );
+};
