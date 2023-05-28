eb95aac9540d5475a6ae5e93beaa72cfc07bcfff
irfan <irfan.mohammad@cognitivebotics.net>
2023-05-28 22:44:58 +0530

    coming soon modified

--- src/sections/ComingSoon.jsx
+++ src/sections/ComingSoon.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function ComingSoon() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
+    <div className="flex flex-col  rounded-lg  items-center justify-center h-screen bg-gradient-to-b from-purple-900 to-gray-900">
       <h1 className="text-5xl font-bold text-white mb-8">
         <span className="text-blue-500">NFT-Metaverse Coming</span> Soon
       </h1>
       <div className="relative w-64 h-64">
-        <div className="moving-graphic">
-        </div>
+        <div className="moving-graphic rounded-full bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-pulse"></div>
       </div>
     </div>
+
   )
 }
 
