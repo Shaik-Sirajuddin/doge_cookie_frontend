2ae5a2ee4355654009a267948cae8a2c896d56eb
irfan <irfan.mohammad@cognitivebotics.net>
2023-05-30 21:59:16 +0530

    roadmap 2.0

--- src/sections/Roadmap.jsx
+++ src/sections/Roadmap.jsx
@@ -7,42 +7,141 @@ import styles from '../styles';
 import { fadeIn, staggerContainer } from '../utils/motion';
 
 function Roadmap() {
-  return (<>
-        <div className="gradient-02 z-0"  />
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
-    ><TypingText title="| Road Map" textStyles="text-center" />
+  return (
+    // <>
+    //       <div className="gradient-02 z-0"  />
+    //   <motion.div
+    //     variants={staggerContainer}
+    //     initial="hidden"
+    //     whileInView="show"
+    //     viewport={{ once: false, amount: 0.25 }}
+    //     className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    //   ><TypingText title="| Road Map" textStyles="text-center" />
 
-    <img
-      src="/images/IMG_20230528_223728_099.jpg"
-      alt="headset"
-      className="img-fluid"
-      style={{ width: '204px', height: '106px', objectFit: 'contain' }}
-    />
-     <motion.p
-        variants={fadeIn('up', 'tween', 0.2, 1)}
-        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
-      >
-        <div className="d-flex flex-column align-items-center">
-            <a  href={require("../assets/doge-cookie-logo.png")}
-                download="myFile">Visit our white paper</a>
-            <div className="d-flex justify-content-center">
-              {/* <a
-                href={require("../assets/doge-cookie-logo.png")}
-                download="myFile"
-                className="btn btn-primary"
-              >
-                Download file
-              </a> */}
-            </div>
+    //   <img
+    //     src="/images/IMG_20230528_223728_099.jpg"
+    //     alt="headset"
+    //     className="img-fluid"
+    //     style={{ width: '204px', height: '106px', objectFit: 'contain' }}
+    //   />
+    //    <motion.p
+    //       variants={fadeIn('up', 'tween', 0.2, 1)}
+    //       className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+    //     >
+    //       <div className="d-flex flex-column align-items-center">
+    //           <a  href={require("../assets/doge-cookie-logo.png")}
+    //               download="myFile">Visit our white paper</a>
+    //           <div className="d-flex justify-content-center">
+    //             {/* <a
+    //               href={require("../assets/doge-cookie-logo.png")}
+    //               download="myFile"
+    //               className="btn btn-primary"
+    //             >
+    //               Download file
+    //             </a> */}
+    //           </div>
+    //         </div>
+    //     </motion.p>
+    //   </motion.div>
+    //   </>
+
+    <div className='bg-black p-10 rounded-lg border-radius '>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="container mx-auto px-4 text-center">
+          <h1 className="text-8xl font-bold text-white mb-8">
+            <span className="text-blue-500">Road</span> Map
+          </h1>
+        </div>
+
+        <div className=" flex items-center">
+          <div className="container mx-auto px-4">
+            <ol className="relative border-l border-gray-200 dark:border-gray-700">
+              <li className="mb-10 ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">1. Token Creation. <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Phase I</span></h3>
+                {/* <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on January 13th, 2022</time> */}
+                {/* <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p> */}
+                {/* <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"><svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clip-rule="evenodd"></path></svg> Download ZIP</a> */}
+              </li>
+              <li className="mb-10 ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">2. $DCK Smart Contract.
+                  {/* <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Phase I</span> */}
+                </h3>
+              </li>
+              <li className="mb-10 ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">3. White-Paper.
+                  {/* <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Phase I</span> */}
+                </h3>
+              </li>
+              <li className="mb-10 ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">4. Website Launch.
+                  {/* <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Phase I</span> */}
+                </h3>
+              </li>
+              <li className="mb-10 ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">5. Set Up Members Dashboard.
+                  {/* <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Phase I</span> */}
+                </h3>
+              </li>
+              <li className="mb-10 ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">6. $DCK Smart Contract Audit.
+                  {/* <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Phase I</span> */}
+                </h3>
+              </li>
+              <li className="mb-10 ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">7. Social Media Presence.
+                  {/* <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Phase I</span> */}
+                </h3>
+              </li>
+              <li className="mb-10 ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">8. Token Presale Launch.
+                  {/* <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Phase I</span> */}
+                </h3>
+              </li>
+              <li className="mb-10 ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">9. Presale Launch-Pad Listing.
+                  {/* <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Phase I</span> */}
+                </h3>
+              </li>
+              <li className="mb-10 ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">10. Promotions.
+                  {/* <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Phase I</span> */}
+                </h3>
+              </li>
+            </ol>
           </div>
-      </motion.p>
-    </motion.div>
-    </>
+        </div>
+      </div>
+    </div>
   );
 }
 
